1bce8da2cda237149f480edfc04860971f5ce709
hojmax <axelhojmark@gmail.com>
2022-12-12 13:31:34 +0100

    Changed benchmarking

--- README.md
+++ README.md
@@ -16,6 +16,16 @@ When using colab, upload the `main.ipynb` notebook. Afterwards, add a cell to th
 
 When pushing to the repository, you need to run `!git pull` and restart the runtime for the changes to take effect.
 
+If you also want to use the benchmarking data you should run the following command as well:
+```python
+!git clone https://git_token@github.com/hojmax/rl-mpsp-benchmark.git
+```
+
+The ```git_token``` should be replaced by your personal access token, and is required since the repo is private. You can generate a token by going to:
+
+Settings -> Developer Settings -> Personal Access Tokens -> Tokens (classic)
+
+
 ## 🏋️ Weights & Biases
 
 You will need an API key when connecting. This can be found in your settings.
--- benchmark.py
+++ benchmark.py
@@ -1,22 +1,43 @@
-import os
+import pandas as pd
 import numpy as np
+import os
 
 
 def get_benchmarking_data(path):
     """Go through all files in the directory and return a list of dictionaries with:
-     N, R, C, seed, transportation_matrix"""
+     N, R, C, seed, transportation_matrix, paper_result"""
+
     output = []
+    df = pd.read_excel(
+        os.path.join(path, "paper_results.xlsx"),
+    )
 
     for file in os.listdir(path):
         if file.endswith(".txt"):
             with open(os.path.join(path, file), "r") as f:
                 lines = f.readlines()
+                N = int(lines[0].split(": ")[1])
+                R = int(lines[1].split(": ")[1])
+                C = int(lines[2].split(": ")[1])
+                seed = int(lines[3].split(": ")[1])
+
+                paper_result = df[
+                    (df['N'] == N) &
+                    (df['R'] == R) &
+                    (df['C'] == C) &
+                    (df['seed'] == seed)
+                ]['res'].values
+
+                assert len(paper_result) == 1
+                paper_result = paper_result[0]
+
                 output.append({
-                    "N": int(lines[0].split(": ")[1]),
-                    "R": int(lines[1].split(": ")[1]),
-                    "C": int(lines[2].split(": ")[1]),
-                    "seed": int(lines[3].split(": ")[1]),
-                    "transportation_matrix": np.loadtxt(lines[4:], dtype=int)
+                    "N": N,
+                    "R": R,
+                    "C": C,
+                    "seed": seed,
+                    "transportation_matrix": np.loadtxt(lines[4:], dtype=int),
+                    "paper_result": paper_result
                 })
 
     return output
